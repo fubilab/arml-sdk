37b0861078969577858b511bb13738eb2f7481fa
Paul Hine <paul.hine@gmail.com>
2024-10-03 17:08:37 +0200

    add arml-utils (recording scripts)

---
+++ arml-utils/record-oak.sh
@@ -0,0 +1,1 @@
+./bin/sai-cli record oak --auto_subfolders --no_rgb --no_preview --recording_only --resolution 800p --output ./out/oak
---
+++ arml-utils/record-orbbec.sh
@@ -0,0 +1,2 @@
+./bin/sai-record-orbbec --exposure 50 --gain 50 --whitebalance 3800 --auto_subfolders --no_preview --frame_rate 15 --recording_only --output ./out/orbbec
+
