1ba3cb3d56754be774fef6745e5aabc42108939d
Paul Hine <paul.hine@gmail.com>
2024-11-13 15:51:35 +0100

    remote control script

---
+++ arml-unity/Assets/ARML/ARMLCore/Scripts/UI/RemoteControl.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class RemoteControl : MonoBehavior {
+    public static RemoteControl Instance;
+
+    public Action OnMenuPress;
+    public Action OnMenuLongPress;
+
+    IEnumerator _MenuButtonPressCoroutine = null;
+
+    IEnumerator MenuButtonPress() {
+        yield return new WaitForSeconds(3);
+        print("Menu long press");
+        _MenuButtonPressCoroutine = null;
+
+    }
+
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Menu))
+        {
+            print("Down: Menu");
+            
+            _MenuButtonPressCoroutine = MenuButtonPress();
+            StartCoroutine(_MenuButtonPressCoroutine);
+        }
+        if (Input.GetKeyUp(KeyCode.Menu))
+        {
+            if (_MenuButtonPressCoroutine != null) {
+                StopCoroutine(_MenuButtonPressCoroutine);
+                _MenuButtonPressCoroutine = null;
+                print("Up: Menu");
+            }
+        }
+    }
+}
