eea1f2a5b73a821c865b1cf1adc80a3e8d7b127e
Paul Hine <paul.hine@gmail.com>
2026-07-15 19:27:27 +0200

    adjusts implementation of IgnoreYPosition
    - moves IgnoreYPosition to PoseProvider
    - adds runtime param for origin Y offset

--- arml-unity/Assets/ARML/ARMLCore/Plugins/SpectacularAI/SDK/Wrappers/DepthAI/Vio.cs
+++ arml-unity/Assets/ARML/ARMLCore/Plugins/SpectacularAI/SDK/Wrappers/DepthAI/Vio.cs
@@ -47,9 +47,6 @@ namespace SpectacularAI.DepthAI
         [Tooltip("Use more lightweight parameters.")]
         public bool FastVio = false;
         
-        [Tooltip("Ignore Y position from the PoseProvider so Camera always stays at the set height.")]
-        public bool IgnoreYPosition = false;
-
         [Tooltip("Native options: 125, 250, 500.")]
         public AccelerometerFrequency AccFrequencyHz = AccelerometerFrequency.Hz500;
 
